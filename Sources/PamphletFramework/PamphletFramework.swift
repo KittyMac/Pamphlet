976239659b2f288fb36410312efb4e57aa54ccb9
Rocco Bowling <rocco@chimerasw.com>
2020-10-18 02:52:49 -0400

    switching to htmlcompressor for html and css only; closure-compiler for js files

--- Sources/PamphletFramework/PamphletFramework.swift
+++ Sources/PamphletFramework/PamphletFramework.swift
@@ -269,8 +269,7 @@ public struct PamphletFramework {
                     }
                 }
                 
-                if inFile.hasSuffix(".js") ||
-                    inFile.hasSuffix(".css") ||
+                if inFile.hasSuffix(".css") ||
                     inFile.hasSuffix(".html") &&
                     FileManager.default.fileExists(atPath: "/usr/local/bin/htmlcompressor") {
                     // If this is a javascript file and closure-compiler is installed
@@ -293,7 +292,33 @@ public struct PamphletFramework {
                             throw ""
                         }
                     } catch {
-                        fatalError("Failed to use /usr/bin/gzip to compress the requested file")
+                        fatalError("Failed to use /usr/local/bin/htmlcompressor to compress the requested file")
+                    }
+                }
+                
+                if inFile.hasSuffix(".js") &&
+                    FileManager.default.fileExists(atPath: "/usr/local/bin/closure-compiler") {
+                    // If this is a javascript file and closure-compiler is installed
+                    do {
+                        let task = Process()
+                        task.executableURL = URL(fileURLWithPath: "/usr/local/bin/closure-compiler")
+                        let inputPipe = Pipe()
+                        let outputPipe = Pipe()
+                        task.standardInput = inputPipe
+                        task.standardOutput = outputPipe
+                        task.standardError = nil
+                        try task.run()
+                        if let fileContentsAsData = fileContents.data(using: .utf8) {
+                            inputPipe.fileHandleForWriting.write(fileContentsAsData)
+                            inputPipe.fileHandleForWriting.closeFile()
+                            let minifiedData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+                            
+                            fileContents = String(data: minifiedData, encoding: .utf8) ?? fileContents
+                        } else {
+                            throw ""
+                        }
+                    } catch {
+                        fatalError("Failed to use /usr/local/bin/closure-compiler to compress the requested file")
                     }
                 }
                 
