483cf5032cdda487a8c13e1e4699fc0bd0734ccb
Rocco Bowling <rocco@chimerasw.com>
2021-01-11 23:06:21 -0500

    adding options: includeOriginal - choose to include the original file content or not includeGzip - choose to include gzip'd file content or not minifyHtml - choose to minify css/html content or not minifyJs - choose to minify javascript content or not minifyTs - choose to compile typescript content or not minifyJson - choose to minify json content or not
    Reworked the file/string generation code to make the above options
    easier.

---
+++ Sources/PamphletFramework/PamphletFramework+GZIP.swift
@@ -0,0 +1,35 @@
+import Foundation
+import Ipecac
+import libmcpp
+
+extension PamphletFramework {
+    func gzip(fileContents: String) -> String? {
+        if options.contains(.includeGzip) {
+            do {
+                let task = Process()
+                task.executableURL = URL(fileURLWithPath: "/usr/bin/gzip")
+                task.arguments = ["-9", "-n"]
+                let inputPipe = Pipe()
+                let outputPipe = Pipe()
+                task.standardInput = inputPipe
+                task.standardOutput = outputPipe
+                task.standardError = nil
+                try task.run()
+                if let fileContentsAsData = fileContents.data(using: .utf8) {
+                    DispatchQueue.global(qos: .userInitiated).async {
+                        inputPipe.fileHandleForWriting.write(fileContentsAsData)
+                        inputPipe.fileHandleForWriting.closeFile()
+                    }
+                    let compressedData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+                    
+                    return compressedData.base64EncodedString()
+                } else {
+                    throw ""
+                }
+            } catch {
+                fatalError("Failed to use /usr/bin/gzip to compress the requested file")
+            }
+        }
+        return nil
+    }
+}
---
+++ Sources/PamphletFramework/PamphletFramework+JSON.swift
@@ -0,0 +1,38 @@
+import Foundation
+import Ipecac
+import libmcpp
+
+extension PamphletFramework {
+    func minifyJson(inFile: String, fileContents: inout String) {
+        if options.contains(.minifyJson) {
+            if (inFile.hasSuffix(".json")) &&
+                FileManager.default.fileExists(atPath: "/usr/local/bin/jj") {
+                // If this is a javascript file and closure-compiler is installed
+                do {
+                    let task = Process()
+                    task.executableURL = URL(fileURLWithPath: "/usr/local/bin/jj")
+                    let inputPipe = Pipe()
+                    let outputPipe = Pipe()
+                    task.standardInput = inputPipe
+                    task.standardOutput = outputPipe
+                    task.standardError = nil
+                    task.arguments = ["-u"]
+                    try task.run()
+                    if let fileContentsAsData = fileContents.data(using: .utf8) {
+                        DispatchQueue.global(qos: .userInitiated).async {
+                            inputPipe.fileHandleForWriting.write(fileContentsAsData)
+                            inputPipe.fileHandleForWriting.closeFile()
+                        }
+                        let minifiedData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+                        
+                        fileContents = String(data: minifiedData, encoding: .utf8) ?? fileContents
+                    } else {
+                        throw ""
+                    }
+                } catch {
+                    fatalError("Failed to use /usr/local/bin/jj to compress the requested file")
+                }
+            }
+        }
+    }
+}
