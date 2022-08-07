8418d63114993fffd363cf816af4a65838d768af
Rocco Bowling <rocco@chimerasw.com>
2022-08-06 23:19:29 -0400

    updating swift argument parser to 1.0.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
 		.package(url: "https://github.com/KittyMac/Hitch.git", from: "0.4.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         .executableTarget(
