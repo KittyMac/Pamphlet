04e518e701ac977980954607ea4d128a526f1245
Rocco Bowling <rocco@chimerasw.com>
2026-05-01 16:24:02 -0400

    add noble binary tool

--- Plugins/PamphletPlugin/PluginShared.swift
+++ Plugins/PamphletPlugin/PluginShared.swift
@@ -44,6 +44,9 @@ func binaryTool(context: PluginContext, named toolName: String) -> String {
         if osFile.contains("jammy") {
             osName = "jammy"
         }
+        if osFile.contains("noble") {
+            osName = "noble"
+        }
     }
     #endif
     
